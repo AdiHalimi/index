6f9c4d28ac6d8afb257ff3475a593c9e59bcc479
AdiHalimi <halimiadi00@gmail.com>
2025-05-03 05:44:57 -0700

    day.22

---
+++ day.22/main.js
@@ -0,0 +1,46 @@
+document.write("hello world");
+
+var x = 10;
+var name = "adi baba";
+
+document.write(x);
+console.log(name);
+
+var a = 50 + 50;
+console.log(a);
+
+var y = 50;
+var z = 80;
+var yz = y + z;
+
+console.log(yz);
+
+var n = 88;
+n++;
+console.log(n);
+
+var o = 40;
+o--;
+console.log(o);
+
+var p = 20;
+var q = p ** 2;
+console.log(q);
+
+var m = 15;
+m+=5; // 15 + 5 = 20
+console.log(m);
+
+var r = 35;
+r*=5;
+console.log(r);
+
+var num1 = 5;
+var num3 = 10;
+var num2 = 5;
+console.log(num1 != num2);
+console.log(num1 == num2);
+console.log(num1 > num2);
+console.log(num1 >= num2);
+console.log(num1 < num2);
+console.log(num1 <= num2);
