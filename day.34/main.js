9595592aa722555eefa08269048deb48dd352bda
AdiHalimi <halimiadi00@gmail.com>
2025-09-13 04:22:05 -0700

    day.33

---
+++ day.34/main.js
@@ -0,0 +1,8 @@
+function printNames(){
+    document.write("Ajani");
+    document.write("<br>");
+    setTimeout(function() {document.write("bleard"); } ,3000);
+    document.write("Dalmat")
+}
+
+printNames();
